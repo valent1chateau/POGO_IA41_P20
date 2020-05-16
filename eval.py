2144c80a6dbb4c57d1336cfb8d799ad746e38029
enmur <stephane.iovleff@gmail.com>
2020-05-16 15:28:09 +0200

    fct eval board

--- eval.py
+++ eval.py
@@ -1,1 +1,19 @@
-print("hello")
+import numpy as np
+
+def evalboard(board,n):
+    eval=0
+    nbTabs=0
+    a,b=np.shape(board)
+    for i in range b:
+        for j in range a:
+            if board[i][j][0]==n:
+                eval+=1
+            else:
+                eval-=1
+        nbTabs+=1
+    if eval == nbTabs:
+        eval= float("inf")
+    elif -eval == nbTabs:
+        eval=float("-inf")
+    
+    return eval
