a9275bc74c4a70865f4ba5f984523500d3f51344
valentin castellon <valent1.castellon@gmail.com>
2020-05-20 12:52:01 +0200

    fonction getplays v1

--- getplays.py
+++ getplays.py
@@ -12,53 +12,69 @@ import copy
 global Board
 
 def finPile(pile):
-    return pile[len(pile)-1]
+    if not (pile):
+        return False
+    else:
+        if (len(pile)==0):
+            return pile[0]
+        else:
+            return pile[len(pile)-1]
 
-def move(pileOrigine,pileCible,nbPions):
-    if(nbPions == 1):
-        pileCible.append(pileOrigine[len(pileOrigine)-1])
-        pileOrigine.pop()
-    elif (nbPions == 2):
-        for i in range(len(pileOrigine)-1,1,-1):
-            pileCible.append(pileOrigine[i])
-        for i in range(2):
-            pileOrigine.pop()
-    else: #nbPions == 3
-        for i in range(len(pileOrigine)-1,0,-1):
-            pileCible.append(pileOrigine[i])
-        for i in range (3):
-            pileOrigine.pop()
+def move(pileOrigine,pileCible,a,b,nbPions):
+    if not (pileOrigine):
+        print("source is empty")
+    elif (pileOrigine):
+        if(nbPions == 1):
+            if(len(pileOrigine) == 0):
+                pileCible.append(Board[a][b])
+                pileOrigine.pop()    
+            else:
+                pileCible.append(pileOrigine[len(pileOrigine)-1])
+                pileOrigine.pop()
+        elif(nbPions == 2):
+            for i in range(len(pileOrigine)-1,1,-1):
+                pileCible.append(pileOrigine[i])
+            for i in range(2):
+                pileOrigine.pop()
+        else: #nbPions == 3
+            for i in range(len(pileOrigine)-1,0,-1):
+                pileCible.append(pileOrigine[i])
+            for i in range (3):
+                pileOrigine.pop()
     return 0
 
-def get_plays(Board, player):
+def get_plays(player):
     plays = []
     play = copy.deepcopy(Board)
     for i in range(3) :
         for j in range(3):
-            if(finPile(Board[i][j],i) == player):
-                if(i != 0):
-                    move(play[i][j],play[i-1][j],1)
-                    plays.append(play)
-                    play = copy.deepcopy(Board)
-                if(i != 2):
-                    move(play[i][j],play[i+1][j],1)
-                    plays.append(play) 
-                    play = copy.deepcopy(Board)
-                if(j != 0):
-                    move(play[i][j],play[i][j-1],1)
-                    plays.append(play)
-                    play = copy.deepcopy(Board)
-                if(j != 2):
-                    move(play[i][j],play[i][j+1],1)
-                    plays.append(play)
-                    play = copy.deepcopy(Board)
+            if(finPile(Board[i][j]) == player):
+                for nbPions in range(1,3): 
+                    if(i != 0):
+                        move(play[i][j],play[i-1][j],i,j,nbPions)
+                        plays.append(play)
+                        play = copy.deepcopy(Board)
+                    if(i != 2):
+                        move(play[i][j],play[i+1][j],i,j,nbPions)
+                        plays.append(play) 
+                        play = copy.deepcopy(Board)
+                    if(j != 0):
+                        move(play[i][j],play[i][j-1],i,j,nbPions)
+                        plays.append(play)
+                        play = copy.deepcopy(Board)
+                    if(j != 2):
+                        move(play[i][j],play[i][j+1],i,j,nbPions)
+                        plays.append(play)
+                        play = copy.deepcopy(Board)
+                        
+    print("done")
     return plays
 
-                        
-                
 
 Board = [[[] for i in range (3)] for j in range(3)]
-Board[0][1] = [0,0,1,0]
-print(Board[0],'\n')
-move(Board[0][1],Board[0][2],3)
-print(Board[0])
+for j in range(3):
+    Board[0][j] = [1,1]
+    Board[1][j] = []
+    Board[2][j] = [0,0]
+                
+print(get_plays(1))
