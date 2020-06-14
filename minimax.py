d576e0f7c851a930dd847400c84a24b6bfc7d8cb
Hadrien Rothea <rothea.hadrien@gmail.com>
2020-06-14 17:17:25 +0200

    bug fix

--- minimax.py
+++ minimax.py
@@ -29,39 +29,38 @@ def minimax(depth, player_id,maximizing_player, board, alpha, beta):
     the best evaluation
 
     """
-  #exit condition 1: arrived at leaf node
-  if(depth == 3):#maximum depth before computer takes too long
-    return evalboard(board,player_id)
+    #exit condition 1: arrived at leaf node
+    if(depth == 3): # maximum depth before computer takes too long
+        return evalboard(board,player_id)
   
-  #exit condition 2: evaluation says game is over
-  if(evalboard(board,player_id) == float("inf") or evalboard(board,player_id)== float("-inf") ):
-      #print(evalboard(board,player_id))
-      return evalboard(board,player_id)
+    #exit condition 2: evaluation says game is over
+    if(evalboard(board,player_id) == float("inf") or evalboard(board,player_id)== float("-inf") ):
+        return evalboard(board,player_id)
   
   
-  best = 0
-  #MAXIMISATION
-  if(maximizing_player):
-    plays = get_plays(player_id,board)
-    best = -10000
-    for play in plays:
-        value= minimax(depth+1,player_id,not(maximizing_player),play,alpha,beta)
-        if value>best:
-            best = value
-        if best>=alpha:
-            alpha = best
-        if alpha>= beta:
-            break
-  #MINIMISATION
-  else:
-    plays = get_plays(1-player_id,board)
-    best = 10000
-    for play in plays:
-        value= minimax(depth+1,player_id,not(maximizing_player),play,alpha,beta)
-        if value<best:
-            best = value
-        if best<=beta:
-            beta = best
-        if alpha>= beta:
-            break
-  return best
+    best = 0
+    #MAXIMISATION
+    if(maximizing_player):
+        plays = get_plays(player_id,board)
+        best = -10000
+        for play in plays:
+            value= minimax(depth+1,player_id,not(maximizing_player),play,alpha,beta)
+            if value>best:
+                best = value
+            if best>=alpha:
+                alpha = best
+            if alpha>= beta:
+                break
+    #MINIMISATION
+    else:
+      plays = get_plays(1-player_id,board)
+      best = 10000
+      for play in plays:
+          value= minimax(depth+1,player_id,not(maximizing_player),play,alpha,beta)
+          if value<best:
+              best = value
+          if best<=beta:
+              beta = best
+          if alpha>= beta:
+              break
+    return best
